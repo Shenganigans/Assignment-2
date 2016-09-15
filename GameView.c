191cca412cf9b5ad221b3106195578f7536a3345
Shenganigans <zhangshengan50@gmail.com>
2016-09-15 20:54:04 +1000

    Add files via upload

--- GameView.c
+++ GameView.c
@@ -6,28 +6,39 @@
 #include "Game.h"
 #include "GameView.h"
 // #include "Map.h" ... if you decide to use the Map ADT
-     
+
 struct gameView {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    int hello;
+    int globalScore; //Current game score
+    Round globalRound; //Current game Round
+    int currentTurn; //Current turn
+    int currentPlayer; //The name says it all
+    int *globalHeath; //The array for the health of the players
+    int *trailLocation; //The array for the trail
+    int *currentLocation; //The array for all the current locations of the players
 };
-     
+
 
 // Creates a new GameView to summarise the current state of the game
 GameView newGameView(char *pastPlays, PlayerMessage messages[])
 {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
     GameView gameView = malloc(sizeof(struct gameView));
-    gameView->hello = 42;
+    gameView->globalHeath = malloc(sizeof(NUM_PLAYERS));
+    gameView->trailLocation = malloc(sizeof(TRAIL_SIZE));
+    gameView->currentLocation = malloc(sizeof(NUM_PLAYERS));
+    //todo
     return gameView;
 }
-     
-     
+
+
 // Frees all memory previously allocated for the GameView toBeDeleted
 void disposeGameView(GameView toBeDeleted)
 {
     //COMPLETE THIS IMPLEMENTATION
-    free( toBeDeleted );
+    free(toBeDeleted->globalHeath);
+    free(toBeDeleted->trailLocation);
+    free(toBeDeleted->currentLocation);
+    free(toBeDeleted);
 }
 
 
@@ -36,36 +47,31 @@ void disposeGameView(GameView toBeDeleted)
 // Get the current round
 Round getRound(GameView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return currentView->globalRound;
 }
 
 // Get the id of current player - ie whose turn is it?
 PlayerID getCurrentPlayer(GameView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return currentView->currentTurn;
 }
 
 // Get the current score
 int getScore(GameView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return currentView->globalScore;
 }
 
 // Get the current health points for a given player
 int getHealth(GameView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return currentView->globalHeath[player];
 }
 
 // Get the current location id of a given player
 LocationID getLocation(GameView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return currentView->currentLocation[player];
 }
 
 //// Functions that return information about the history of the game
