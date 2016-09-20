72861888783ab12c5aa5eb3cdd6c0d9e1095ae94
neilbaksi <snb.1996@gmail.com>
2016-09-20 23:33:12 +1000

    completed hunterView.c i think :P

--- HunterView.c
+++ HunterView.c
@@ -4,13 +4,19 @@
 #include <assert.h>
 #include "Globals.h"
 #include "Game.h"
-#include "GameView.h"
 #include "HunterView.h"
-// #include "Map.h" ... if you decide to use the Map ADT
+#include "GameView.h"
+#include "Map.h" // if you decide to use the Map ADT
+
+#define STARTING_ROUND 0
+#define NO_OF_ACTIONS_PER_TURN 8
+//366 is mai Dracula turns
+#define MAi_TURNS (366*5)
+#define MAi_LENGTH ( MAi_TURNS * (NO_OF_ACTIONS_PER_TURN + 1) )
      
 struct hunterView {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    int hello;
+     GameView hunter;
+     LocationID trailLocation[TRAIL_SIZE];
 };
      
 
@@ -19,7 +25,46 @@ HunterView newHunterView(char *pastPlays, PlayerMessage messages[])
 {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
     HunterView hunterView = malloc(sizeof(struct hunterView));
-    hunterView->hello = 42;
+    hunterView->hunter = newGameView(pastPlays, messages);
+
+    int i;
+
+    for( i = 0 ; i < TRAIL_SIZE ; i++ ){
+        hunterView->trailLocation[i] = UNKNOWN_LOCATION;
+    }
+
+    int past = strnlen(pastPlays, MAi_LENGTH);
+
+    for( i = 0 ; i < past ; i += NO_OF_ACTIONS_PER_TURN){
+        PlayerID currPlayer = ( (i/(NO_OF_ACTIONS_PER_TURN)) % NUM_PLAYERS);
+
+        if(currPlayer == PLAYER_DRACULA){ // Getting Dracula Information
+            LocationID currentLocation = abbrevToID(pastPlays + i + 1);
+
+            if(!validPlace(currentLocation)){
+                if(pastPlays[i+1] == 'H'){ // Hide Move
+                    currentLocation = hunterView->trailLocation[0];
+                } else if(pastPlays[i+1] == 'D'){ // Double Back Move
+                    int back = (int)(pastPlays[i+1] - '0');
+                    currentLocation = hunterView->trailLocation[back];
+                } else if(pastPlays[i+1] == 'T'){ // TP to Castle Dracula
+                    currentLocation = CASTLE_DRACULA;
+                } else if(pastPlays[i+1] == 'C'){ // City Move
+                    currentLocation = CITY_UNKNOWN;
+                } else if(pastPlays[i+1] == 'S'){ // Sea Move
+                    currentLocation = SEA_UNKNOWN;
+                } else {
+                    currentLocation = UNKNOWN_LOCATION;
+                }
+            }
+
+            int j;
+            for(j = (TRAIL_SIZE - 1) ; j >= 1 ; j--) {
+                hunterView->trailLocation[j] = hunterView->trailLocation[j-1]; // Moving hunter along the trail
+            }
+            hunterView->trailLocation[0] = currentLocation;
+        }
+    }
     return hunterView;
 }
      
@@ -37,36 +82,31 @@ void disposeHunterView(HunterView toBeDeleted)
 // Get the current round
 Round giveMeTheRound(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getRound(currentView->hunter);
 }
 
 // Get the id of current player
 PlayerID whoAmI(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getCurrentPlayer(currentView->hunter);
 }
 
 // Get the current score
 int giveMeTheScore(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getScore(currentView->hunter);
 }
 
 // Get the current health points for a given player
 int howHealthyIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getHealth(currentView->hunter, player);
 }
 
 // Get the current location id of a given player
 LocationID whereIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getLocation(currentView->hunter, player);
 }
 
 //// Functions that return information about the history of the game
@@ -75,23 +115,80 @@ LocationID whereIs(HunterView currentView, PlayerID player)
 void giveMeTheTrail(HunterView currentView, PlayerID player,
                             LocationID trail[TRAIL_SIZE])
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    getHistory(currentView->hunter, player, trail);
 }
+
 
 //// Functions that query the map to find information about connectivity
 
-// What are my possible next moves (locations)
-LocationID *whereCanIgo(HunterView currentView, int *numLocations,
-                        int road, int rail, int sea)
+// What are my possible neit moves (locations)
+LocationID *whereCanIgo(HunterView currentView, int *numLocations, int road, int rail, int sea)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return NULL;
+    LocationID *possibilites;
+
+    if(giveMeTheRound(currentView) == STARTING_ROUND){
+        possibilites = (LocationID *)(malloc(sizeof(LocationID)*NUM_MAP_LOCATIONS));
+        *numLocations = 0;
+
+        int i;
+        for( i = 0 ; i<NUM_MAP_LOCATIONS ; i++ ){
+            possibilites[i] = i;
+        }
+    } else {
+        possibilites = connectedLocations(currentView->hunter, numLocations,
+                                        whereIs(currentView,
+                                                getCurrentPlayer(currentView->hunter)),
+                                        getCurrentPlayer(currentView->hunter),
+                                        giveMeTheRound(currentView), road , rail,
+                                        sea);
+    }
+
+
+    return possibilites;
 }
 
-// What are the specified player's next possible moves
+// What are the specified player's neit possible moves
 LocationID *whereCanTheyGo(HunterView currentView, int *numLocations,
                            PlayerID player, int road, int rail, int sea)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return NULL;
+    Round turn;
+
+    if( whoAmI(currentView) <= player){
+        turn = giveMeTheRound(currentView);
+    } else {
+        turn = (giveMeTheRound(currentView) + 1) ;
+    }
+
+    LocationID *possibilites;
+
+    if( turn == STARTING_ROUND){
+        possibilites = (LocationID *)(malloc(sizeof(LocationID)*NUM_MAP_LOCATIONS));
+
+        int i;
+        for( i = 0 ; i < NUM_MAP_LOCATIONS ; i++ ) {
+            if( player != PLAYER_DRACULA || i != ST_JOSEPH_AND_ST_MARYS){
+                possibilites[i] = i;
+            }
+        }
+    } else {
+        if( PLAYER_DRACULA == player){
+            LocationID dracLocation = whereIs( currentView, PLAYER_DRACULA);
+
+            if(validPlace(dracLocation)){
+                possibilites = connectedLocations(currentView->hunter, numLocations,
+                                                whereIs(currentView, PLAYER_DRACULA),
+                                                turn, player, road,
+                                                FALSE, sea); // can't travel by rail
+            } else {
+                possibilites = NULL; 
+            }
+        } else {
+            possibilites = connectedLocations( currentView->hunter, numLocations,
+                                            whereIs(currentView, player),
+                                            turn, player, road, rail,
+                                            sea);
+        }
+    }
+
+    return possibilites;
 }
