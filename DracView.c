78f70c8ce15da31190ff2a4802df6fcd50c5c463
Shenganigans <zhangshengan50@gmail.com>
2016-09-22 19:28:24 +1000

    Update DracView.c

--- DracView.c
+++ DracView.c
@@ -7,7 +7,14 @@
 #include "GameView.h"
 #include "DracView.h"
 // #include "Map.h" ... if you decide to use the Map ADT
-
+#define CHARS_PER_TURN 8
+#define LOCATION_ABBREVIATION 1
+#define TRAP 3
+#define VAMP 4
+#define DRACULA_ACTION 5
+#define START_ENCOUNTER 3
+#define MAX_ENCOUNTERS 3
+static void insertdracTrailLoc (DracView dracView, LocationID placeID);
 struct dracView {
     GameView draculaView;
     LocationID dracTrail[TRAIL_SIZE];
@@ -21,7 +28,67 @@ DracView newDracView(char *pastPlays, PlayerMessage messages[])
 {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
     DracView dracView = malloc(sizeof(struct dracView));
-    dracView->draculaView = newGameView(pastPlays,messages);
+    dracView->draculaView = newGameView(pastPlays, messages);
+
+    int i, j;
+    for(i = 0; i < dracTrail_SIZE; i++) {
+        dracView->dracTrail[i] = UNKNOWN_LOCATION;
+    }
+
+    for(i = 0;i < NUM_MAP_LOCATIONS; i++) {
+        dracView->traps[i] = 0;
+    }
+    dracView->location = NOWHERE;
+
+    int past = strnlen(pastPlays, MAX_PAST_PLAY_LENGTH);
+
+    for(i = 0; i < past; i += CHARS_PER_TURN) {
+        PlayerID currPlayer = ( (i/CHARS_PER_TURN) % NUM_PLAYERS);
+        LocationID currLoc = abbrevToID(pastPlays + i + LOCATION_ABBREVIATION);
+        if(!validPlace(currLoc)) {
+            switch (pastPlays[i + LOCATION_ABBREVIATION]) {
+					 case 'H':
+						 currLoc = dracView->dracTrail[RECENT_LOCATION];
+					    break;
+					 case 'T':
+					    currLoc = CASTLE_DRACULA;
+						 break;
+					 case 'D':
+						 currLoc = dracView->dracTrail[pastPlays[i + LOCATION_ABBREVIATION + 1]-'0'];
+						 break;
+					 default:
+						 assert(TRUE == FALSE);
+				}
+        }
+
+        assert(validPlace(currLoc));
+
+        if(currPlayer == PLAYER_DRACULA) {
+            if(pastPlays[i + TRAP] == 'T') {
+                dracView->traps[currLoc]++;
+            }
+            if(pastPlays[i + VAMP] == 'V') {
+                dracView->location = currLoc;
+            }
+            if(pastPlays[i + DRACULA_ACTION] == 'M') {
+                dracView->traps[dracView->dracTrail[dracTrail_SIZE - 1]]--;
+            } else if(pastPlays[i + DRACULA_ACTION] == 'V') {
+                dracView->location = NOWHERE;
+            }
+            insertdracTrailLoc(dracView, currLoc);
+        } else {
+            for(j = 0; j < MAX_ENCOUNTERS; j++) {
+                char encounter = pastPlays[i + START_ENCOUNTER + j];
+
+                if(encounter == 'T') {
+                    dracView->traps[currLoc]--;
+                } else if(encounter == 'V') {
+                    dracView->location= NOWHERE;
+                }
+            }
+        }
+    }
+
     return dracView;
 }
 
@@ -77,8 +144,16 @@ void lastMove(DracView currentView, PlayerID player,
 void whatsThere(DracView currentView, LocationID where,
                          int *numTraps, int *numVamps)
 {
-    
-    return;
+    assert(currentView != NULL);
+    assert(validPlace(where));
+
+    if(where == currentView->location) {
+        (*numVamps) = 1;
+    } else {
+        (*numVamps) = 0;
+    }
+
+    (*numTraps) = currentView->trap[where];
 }
 
 //// Functions that return information about the history of the game
@@ -108,4 +183,16 @@ LocationID *whereCanTheyGo(DracView currentView, int *numLocations,
   return (connectedLocations(currentView->draculaView, numLocations,
                                (getLocation(currentView->draculaView, player)), player , (getRound(currentView->draculaView)),
                                road, rail, sea));
+}
+
+static void insertdracTrailLoc (DracView dracView, LocationID placeID) {
+    assert(dracView != NULL);
+    assert(validPlace(placeID));
+
+    int i;
+    for(i = TRAIL_SIZE - 1; i >= 1; i--) {
+        dracView->trail[i] = dracView->trail[i-1];
+    }
+    dracView->trail[0] = placeID;
+    return;
 }
